94a747d751c70bd7ef49460904bb4fe182835efc
Areo-Joe <1047726015@qq.com>
2025-08-05 12:32:10 +0800

    chore: refactor

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,29 @@
 pub mod ai;
 pub mod state;
 use std::env;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::async_runtime::Mutex;
+use tauri::{async_runtime::block_on, Manager};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let conn = block_on(async {
+        turso::Builder::new_local("sqlite.db")
+            .build()
+            .await
+            .unwrap()
+            .connect()
+            .unwrap()
+    });
+
+    block_on(async { rside::words::ensure_table_exists(&conn).await });
+
     tauri::Builder::default()
+        .setup(|app| {
+            app.manage(Mutex::new(state::AppState { conn }));
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, ai::stream_ai])
+        .invoke_handler(tauri::generate_handler![ai::stream_ai])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::async_runtime::Mutex;
-use tauri::{async_runtime::block_on, Builder, Manager};
-use word_ai_lib::state::AppState;
 
 fn main() {
-    let db = block_on(async {
-        turso::Builder::new_local("sqlite.db")
-            .build()
-            .await
-            .unwrap()
-    });
-
-    let conn = db.connect().unwrap();
-    block_on(async { rside::words::ensure_table_exists(&conn).await });
-
-    Builder::default()
-        .setup(|app| {
-            app.manage(Mutex::new(AppState { conn }));
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![word_ai_lib::ai::stream_ai])
-        .run(tauri::generate_context!())
-        .unwrap();
+    word_ai_lib::run();
 }
