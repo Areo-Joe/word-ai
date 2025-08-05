a051c0ff061c7956444ae6bbfbe544fc0b2e5f34
Areo-Joe <1047726015@qq.com>
2025-08-06 02:02:14 +0800

    chore: refactor with regex

--- src/components/linkMaker.tsx
+++ src/components/linkMaker.tsx
@@ -1,47 +1,22 @@
 import { Link } from "@tanstack/react-router";
 
-export function LinkMaker({ text }: { text: string }) {
-  const arr = text
-    .split("")
-    .map((char) =>
-      /[a-zA-Z]+/g.test(char) ? { letter: char } : { content: char },
-    )
-    .reduce(
-      (
-        acc: Array<{ word: string } | { content: string }>,
-        cur: { letter: string } | { content: string },
-      ) => {
-        if ("content" in cur) {
-          acc.push(cur);
-          return acc;
-        }
+export function LinkMaker({ stringArr }: { stringArr: Array<string> }) {
+  const wordReg = /[a-zA-Z]+/g;
+  const wordAndNonWordReg = /[a-zA-Z]+|[^a-zA-Z]+/g;
 
-        if (acc.length === 0) {
-          return [{ word: cur.letter }];
-        } else {
-          const last = acc[acc.length - 1];
-          if ("word" in last) {
-            last.word += cur.letter;
-          } else {
-            acc.push({ word: cur.letter });
-          }
-        }
-        return acc;
-      },
-      [],
-    );
+  const wordAndNonWord = stringArr.join("").match(wordAndNonWordReg)!;
 
-  return arr.map((x) =>
-    "word" in x ? (
+  return wordAndNonWord.map((x) => {
+    return wordReg.test(x) ? (
       <Link
         className="text-slate-900 font-medium hover:text-slate-700 hover:underline"
         to="/word/$word"
-        params={{ word: x.word }}
+        params={{ word: x }}
       >
-        {x.word}
+        {x}
       </Link>
     ) : (
-      <>{x.content}</>
-    ),
-  );
+      <>{x}</>
+    );
+  });
 }
